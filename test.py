b78685ae776dd15e9163317a9a3aace1d6aea28c
hoangtung01022003 <hoangtung01022003@gmail.com>
2026-07-31 17:38:23 +0700

    remove debug files

--- test.py
+++
@@ -1,7 +0,0 @@
-from app.db import fetch_all
-from dotenv import load_dotenv
-load_dotenv()
-try:
-    print(fetch_all("SELECT column_name FROM information_schema.columns WHERE table_name = 'translations'"))
-except Exception as e:
-    print(e)
